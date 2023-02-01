2cfda9ee58f8ad16bc16b405d13d699e2928e89f
ngangakevin <ngangakelvin254@gmail.com>
2023-02-01 11:47:37 +0300

    Notification service bootstrap refactor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+  app.connectMicroservice({
+    transport: Transport.REDIS,
+    options: {
+      port: parseInt(process.env.REDIS_PORT),
+      host: process.env.REDIS_HOST,
+      retryAttempts: parseInt(process.env.REDIS_CONNECTION_RETRY),
+      retryDelay: parseInt(process.env.REDIS_CONNECTION_RETRY_BACKOFF),
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(process.env.HTTP_PORT);
 }
 bootstrap();
